02d3bb6923533a0c0344c1e4fb7b3da3e790b0e1
dima brodski <dima001@gmail.com>
2018-03-24 15:50:16 +0300

    first commit

---
+++ drawview.swift
@@ -0,0 +1,21 @@
+//
+//  drawview.swift
+//  drowing
+//
+//  Created by dima brodski on 27/11/2016.
+//  Copyright © 2016 dima brodski. All rights reserved.
+//
+
+import UIKit
+
+class drawview: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
